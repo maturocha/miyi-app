3da97fd0f2d4fed47c09c57d51d79f0c9c6feaee
Matías <matu.rocha@gmail.com>
2021-08-11 10:32:12 -0300

    fix: Add more time token expiration

--- app/Utils/Helper.php
+++ app/Utils/Helper.php
@@ -31,7 +31,7 @@ if (! function_exists('_token_payload')) {
         return [
             'auth_token' => $authToken,
             'token_type' => 'bearer',
-            'expires_in' => auth()->guard('api')->factory()->getTTL() * 60
+            'expires_in' => auth()->guard('api')->factory()->getTTL() * 9600
         ];
     }
 }
